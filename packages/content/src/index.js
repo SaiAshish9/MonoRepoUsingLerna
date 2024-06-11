1b52ab0b3d32080ee474bea6df72f5831cf1e217
saiashish9 <saiashish3760@gmail.com>
2024-06-11 23:14:13 +0530

    k

--- packages/content/src/index.js
+++ packages/content/src/index.js
@@ -2,13 +2,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "header"; 
+import Header from "header";
 
 const App = () => (
   <div>
     <Header />
     <h1>Content Microfrontend</h1>
   </div>
-); 
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
